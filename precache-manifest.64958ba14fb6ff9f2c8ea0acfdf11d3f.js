d632d27b2af90b25179ab82d36ac1ba8863fd108
SameerShaikh123 <66582622+SameerShaikh123@users.noreply.github.com>
2020-10-01 03:36:44 -1200

    first

---
+++ precache-manifest.64958ba14fb6ff9f2c8ea0acfdf11d3f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bdbeaca1fea9cf13f7ff960740b54a3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d2d7068adb0a8c5ad1c8",
+    "url": "./static/css/main.e2b6ad20.chunk.css"
+  },
+  {
+    "revision": "a35abeb8e3106c99dbba",
+    "url": "./static/js/2.9ab791be.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.9ab791be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2d7068adb0a8c5ad1c8",
+    "url": "./static/js/main.4378e638.chunk.js"
+  },
+  {
+    "revision": "1835b8375ddbc48c0411",
+    "url": "./static/js/runtime-main.ad6d7bcd.js"
+  },
+  {
+    "revision": "4143d240af520e4a8b565060c6e6277b",
+    "url": "./static/media/img-2.4143d240.jpg"
+  },
+  {
+    "revision": "d211c298f5dddd6d141ac9ebee92d8a1",
+    "url": "./static/media/img-8.d211c298.jpg"
+  }
+]);
